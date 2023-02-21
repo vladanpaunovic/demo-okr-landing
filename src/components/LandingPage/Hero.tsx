d3cd2717ba2999071a672708107fd64f997c5a43
Vladan Paunovic <vladan.paunovic.bg@gmail.com>
2023-02-21 19:56:57 +0100

    chore fix typo

--- src/components/LandingPage/Hero.tsx
+++ src/components/LandingPage/Hero.tsx
@@ -4,7 +4,7 @@ export default function Hero() {
   return (
     <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 pt-20 pb-16 text-center lg:pt-32">
       <h1 className="mx-auto max-w-4xl font-display text-5xl font-medium tracking-tight text-slate-900 sm:text-7xl">
-        Realize{" "}
+        Realise{" "}
         <span className="relative whitespace-nowrap text-red-500">
           <svg
             aria-hidden="true"
